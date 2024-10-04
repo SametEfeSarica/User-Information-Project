4f8d18eb6fe244a89211ad79c0e4f8996ed1a77b
Samet Efe Sarıca <sametefes63@gmail.com>
2024-10-05 02:20:42 +0300

    Create Kullanıcı Bilgileri.cs

---
+++ Kullanıcı Bilgileri.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KullanıcıBilgisiAlmaOdevi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Kullanıcıdan Ad, Soyad, Öğrenci No ve Cep Tel No alınacak.
+
+
+            Console.WriteLine("Adınızı Yazın:");
+
+            string ad = Console.ReadLine();
+
+            Console.WriteLine("Soyadınızı Yazın:");
+            
+            string soyad = Console.ReadLine();
+            
+            Console.WriteLine("Öğrenci No'nuzu Yazın:");
+            
+            string ogrenciNo = Console.ReadLine();
+            
+            Console.WriteLine("Cep Telefon Numaranızı Yazın Yazın:");
+
+            string cepTel = Console.ReadLine();
+
+
+            // Alınan Bilgiler Birlikte Görüntülecek.
+
+
+            Console.WriteLine("KULLANICI BİLGİLERİ:");
+            Console.WriteLine("Kullanıcı Adı: " +ad);
+            Console.WriteLine("Kullanıcı Soyadı: " +soyad);
+            Console.WriteLine("Kullanıcı Öğrenci No: " +ogrenciNo);
+            Console.WriteLine("Kullanıcı Cep Telefonu Numarası: " +cepTel);
+
+            //Terminalin ekranda Kalması için.
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
